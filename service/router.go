481693b36d13c67c62ba3a8c5b7e5e0a76ef44ba
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-02-12 14:07:13 +0100

    fix: linter errors
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- service/router.go
+++ service/router.go
@@ -42,7 +42,7 @@ func (r2p *R2PService) postMintRequest(c *gin.Context) {
 
 	valid, err := r2p.VerifyMessage(requestBody)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("error while verifying signature: %s", err.Error())})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error while verifying signature: " + err.Error()})
 		return
 	}
 	if !valid {
@@ -53,7 +53,7 @@ func (r2p *R2PService) postMintRequest(c *gin.Context) {
 	// check whether mint request already exists
 	mr, err := r2p.pmClient.CheckMintRequest(requestBody.Conversion.LiquidTxHash)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("error while fetching mint request: %s", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error while fetching mint request: " + err.Error()})
 		return
 	}
 
@@ -67,25 +67,25 @@ func (r2p *R2PService) postMintRequest(c *gin.Context) {
 	url := fmt.Sprintf("http://%s:%s@%s/wallet/%s", cfg.RPCUser, cfg.RPCPass, cfg.RPCHost, cfg.Wallet)
 	tx, err := r2p.eClient.GetTransaction(url, []string{requestBody.Conversion.LiquidTxHash})
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("error while fetching liquid tx: %s", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error while fetching liquid tx: " + err.Error()})
 		return
 	}
 
 	addresses, err := r2p.eClient.DeriveAddresses(url, []string{requestBody.Conversion.Descriptor})
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("error while deriving liquid addresses: %s", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error while deriving liquid addresses: " + err.Error()})
 		return
 	}
 
 	if !slices.Contains(addresses, tx.Details[0].Address) {
-		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("transaction details do not contain address derived from descriptor: %s", requestBody.Conversion.Descriptor)})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "transaction details do not contain address derived from descriptor: " + requestBody.Conversion.Descriptor})
 		return
 	}
 
 	// return error if reissuance asset is not in liquid tx
 	amt, ok := tx.Amount[cfg.AcceptedAsset]
 	if !ok {
-		c.JSON(http.StatusConflict, gin.H{"error": fmt.Sprintf("tx does not contain accepted asset: %s", cfg.AcceptedAsset)})
+		c.JSON(http.StatusConflict, gin.H{"error": "tx does not contain accepted asset: " + cfg.AcceptedAsset})
 		return
 	}
 
@@ -98,6 +98,6 @@ func (r2p *R2PService) postMintRequest(c *gin.Context) {
 	plmntAmount := r2p.getConversion(uint64(amt))
 	err = r2p.pmClient.MintPLMNT(requestBody.Conversion.Beneficiary, plmntAmount, requestBody.Conversion.LiquidTxHash)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("error while minting token: %s", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error while minting token: " + err.Error()})
 	}
 }
